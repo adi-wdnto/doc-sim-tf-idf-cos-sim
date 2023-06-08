67b4c9eb71c426c6595cd7add7cb30d61e92bf0a
Adi Widianto <77193950+adi-wdnto@users.noreply.github.com>
2023-06-08 18:55:44 +0700

    Add src folder and edited setup.py
    For installation of https://github.com/IBM/transition-amr-parser

---
+++ src/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+import sys
+
+VERSION = '0.5.4'
+
+install_requires = [
+    "torch==1.13.1",
+    'numpy<=1.23.5',
+    'ipython<=8.12', # python 3.8 vs ipython 8.13 incompatibility
+    'tqdm>=4.55.0',
+    'packaging>=20.8',
+    'requests>=2.25.1',
+    # for data (ELMO embeddings)
+    'h5py>=3.0.0',
+    'python-dateutil>=2.8.1',
+    # for scoringy
+    'penman>=1.1.0',
+    # needs tools to be importable > 1.0.4. As of now, no official release
+    'smatch',
+    # for debugging
+    'ipdb',
+    'line_profiler>=4.0.2',
+    'pyinstrument>=4.4.0',
+    # for aws download
+    'boto3>=1.26.1',
+    'progressbar',
+]
+
+# platform dependent fairseq version
+if sys.platform == 'darwin':
+    install_requires.append("fairseq==0.10.0")
+else:
+    install_requires.append("fairseq==0.10.0")
+
+if __name__ == '__main__':
+    setup(
+        name='transition_amr_parser',
+        version=VERSION,
+        description="Trasition-based neural parser",
+        package_dir={"": "src"},
+        # packages=['fairseq_ext', 'transition_amr_parser'],
+        # packages=['neural_parser'],
+        packages=find_packages("src", exclude=('cenv_*', 'configs', 'tests', 'DATA','dist','docker','run','scripts','service','*egg-info')),
+        package_data={'': ['*.txt', '*.md', '*.opt', '*.cu', '*.cpp']},
+        entry_points={
+            'console_scripts': [
+                'amr-parse = transition_amr_parser.parse:main',
+                'amr-machine = transition_amr_parser.amr_machine:main',
+            ]
+        },
+        py_modules=['fairseq_ext', 'transition_amr_parser',"ibm_neural_aligner"],
+        install_requires=install_requires,
+        classifiers=[
+            "Programming Language :: Python :: 3.8",
+            "License :: OSI Approved :: Apache Software License",
+            "Topic :: Scientific/Engineering :: Artificial Intelligence",
+            "Natural Language :: English",
+        ],
+    )
+
